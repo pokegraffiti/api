6f7da5b06354618b9f32bc9098e1ab2f62ae573e
Zane Wolfgang Pickett <sirwolfgang@users.noreply.github.com>
2016-07-13 20:51:13 -0700

    Fix issue with thats not being floating point

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -14,10 +14,10 @@ class OrdersController < ApplicationController
 
   # GET /orders/stats
   def stats
-    total_count   = Order.sum(:sticker_quantity)
-    team_instinct = [Order.team_instinct.sum(:sticker_quantity), 1].max / [total_count, 1].max
-    team_mystic   = [Order.team_mystic.sum(:sticker_quantity),   1].max / [total_count, 1].max
-    team_valor    = [Order.team_valor.sum(:sticker_quantity),    1].max / [total_count, 1].max
+    total_count   = Order.sum(:sticker_quantity).to_f
+    team_instinct = [Order.team_instinct.sum(:sticker_quantity).to_f, 1.0].max / [total_count, 1.0]
+    team_mystic   = [Order.team_mystic.sum(:sticker_quantity).to_f,   1.0].max / [total_count, 1.0]
+    team_valor    = [Order.team_valor.sum(:sticker_quantity).to_f,    1.0].max / [total_count, 1.0]
     render json: {
       instinct: team_instinct,
       mystic: team_mystic,
