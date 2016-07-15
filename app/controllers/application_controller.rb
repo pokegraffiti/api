f31181785a58e5ad2380daca77cbc6e7f5d53869
Zane Wolfgang Pickett <sirwolfgang@users.noreply.github.com>
2016-07-14 22:13:13 -0700

    Covert to a full rails app, Create landing page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,22 +1,18 @@
 # frozen_string_literal: true
-class ApplicationController < ActionController::API
+class ApplicationController < ActionController::Base
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
   rescue_from ActionController::BadRequest, with: :bad_request
   rescue_from ActionController::ParameterMissing, with: :bad_request
 
-  before_action do
-    params.transform_keys!(&:underscore)
-  end
-
   private
 
   def bad_request(error)
-    # Airbrake.notify(error)
+    notify_airbrake(error)
     render json: { error: error.message }, status: :bad_request
   end
 
   def record_not_found(error)
-    # Airbrake.notify(error)
+    notify_airbrake(error)
     render json: { error: error.message }, status: :not_found
   end
 end
